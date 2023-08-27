018faf9610dd1958e9b8b5f7d481feae1e184346
Utkarsh gupta <utkarshgupta4224@gmail.com>
2023-08-28 00:31:38 +0530

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.*;
 public class Main {
+    static class Course{
+        String name;
+        List<Course> requirement;
+        int academicload;
+
+        public Course(String name,int academicload) {
+            this.name = name;
+            this.requirement = new ArrayList<>();
+            this.academicload = academicload;
+        }
+    }
+
+    static class Period{
+        List<Course> courses;
+        int academicload;
+        public Period(){
+            this.courses = new ArrayList<>();
+            this.academicload = 0;
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello, I am utkarsh");
     }
 }
