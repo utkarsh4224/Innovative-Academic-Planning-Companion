c7ec9217e351a01501c43b5cffe822c78f1c3079
Utkarsh gupta <utkarshgupta4224@gmail.com>
2023-08-28 23:37:15 +0530

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,28 @@
 import java.util.*;
 public class Main {
-    static class Course{
-        String name;
-        List<Course> requirement;
-        int academicload;
+    static class Subject{ //indivitual coruse ko assine krne ke liye , taki Subject re related cheejo ko manage krne mai asani ho
+        String name; //Subject ka naam
+        List<Subject> requirement; //Subject ko krne ke liye requirement (means agr vo DSA ka Subject krna chahta hai to usse pehle koi language subject padha hona chahiye)
+        int credits;// kitna us Subject ka credit hai
 
-        public Course(String name,int academicload) {
+        public Subject(String name,int credits) { //parameterised constructor
             this.name = name;
             this.requirement = new ArrayList<>();
-            this.academicload = academicload;
+            this.credits = credits;
         }
     }
 
-    static class Period{
-        List<Course> courses;
-        int academicload;
+    static class Period{ 
+        List<Subject> Subjects;//Subjects ki list is period ke liye
+        int credits;//is period mai kitne credits hue
         public Period(){
-            this.courses = new ArrayList<>();
-            this.academicload = 0;
+            this.Subjects = new ArrayList<>();//list of subjects for this period
+            this.credits = 0;//initializing credit value to zero
         }
     }
 
     static class ScheduleGenerator{
-        List<Course> courses;
+        List<Subject> Subjects;
 
     }
 
