cebf91d3ac390c46749f09a76f0af5e50214baf3
Utkarsh gupta <utkarshgupta4224@gmail.com>
2023-08-29 23:52:54 +0530

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,57 @@
 import java.util.*;
+
 public class Main {
-    static class Subject{ //indivitual coruse ko assine krne ke liye , taki Subject re related cheejo ko manage krne mai asani ho
-        String name; //Subject ka naam
-        List<Subject> requirement; //Subject ko krne ke liye requirement (means agr vo DSA ka Subject krna chahta hai to usse pehle koi language subject padha hona chahiye)
-        int credits;// kitna us Subject ka credit hai
-        public Subject(String name,int credits) { //parameterised constructor
+    static class Course{ //indivitual coruse ko assine krne ke liye , taki Course re related cheejo ko manage krne mai asani ho
+        String name; //Course ka naam
+        List<Course> requirements; //Course ko krne ke liye requirement (means agr vo DSA ka Course krna chahta hai to usse pehle koi language Course padha hona chahiye)
+        int credits;// kitna us Course ka credit hai
+        public Course(String name,int credits) { //parameterised constructor
             this.name = name;
-            this.requirement = new ArrayList<>();
+            this.requirements = new ArrayList<>();
             this.credits = credits;
+        }
+
+        public String getName(){
+            return name;
+        }
+
+        public int getCredits(){
+            return credits;
+        }
+
+        public void addRequirement(Course requirement){
+            requirements.add(requirement);
+        }
+
+        public List<Course> getRequirements(){
+            return requirements;
         }
     }
 
     static class Period{ 
-        List<Subject> Subjects;//Subjects ki list is period ke liye
-        int credits;//is period mai kitne credits hue
+        List<Course> courses;//Courses ki list is period ke liye
+        int Totalcredits;//is period mai kitne credits hue
         public Period(){
-            this.Subjects = new ArrayList<>();//list of subjects for this period
-            this.credits = 0;//initializing credit value to zero
+            this.courses = new ArrayList<>();//list of Courses for this period
+            this.Totalcredits = 0;//initializing credit value to zero
+        }
+
+        public void addCourse(Course course) {
+            courses.add(course);
+            Totalcredits += course.getCredits();
+        }
+
+        public List<Course> getCourses() {
+            return courses;
+        }
+
+        public int getTotalCredits() {
+            return Totalcredits;
         }
     }
 
     static class ScheduleGenerator{
-        List<Subject> Subjects;
+        List<Course> Courses;
 
     }
 
